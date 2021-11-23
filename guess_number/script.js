2b6ef15b2b8bb799c40de7ca3a66a9649ba6d6d7
0abramov0 <76647543+0abramov0@users.noreply.github.com>
2021-11-23 19:47:43 +0500

    Add files via upload

---
+++ guess_number/script.js
@@ -0,0 +1,40 @@
+let userName = '';
+let num = Math.floor(Math.random()*100);
+let out = document.querySelector('#out');
+let form = document.querySelector('#form');
+let input = document.querySelector('#form input');
+
+form.addEventListener('submit', eventHandler);
+message('Введите ваше имя');
+
+
+function message(message){
+  let li = document.createElement('li');
+  li.textContent = message;
+  out.appendChild(li);
+}
+
+function eventHandler(event){
+  event.preventDefault();
+  game(input.value);
+  input.value  = '';
+}
+
+function game (input){
+  if (!userName){
+    userName = input;
+    message(input+ ' приветствую тебя. Предлагаю тебе сыграть в игру, я загадаю число от 1 до 100, а тебе нужно будет его отгадать.');
+    return;
+  }
+  let n = Number.parseInt(input);
+  message(n);
+  if (n>num){
+    message('Слишком много, попробую еще раз');
+  }else if (n<num){
+    message('Слишком мало, попробуй еще раз');
+  }else if (n == num){
+    message('Отлично! Ты угадал мое число ');
+  }
+}
+
+  
